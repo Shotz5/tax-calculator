7ed04358c6792fc1d49d8d0dc8686eab38d8c4ee
Alex Kitsul <kitsulalex@gmail.com>
2024-11-29 18:03:27 -0800

    Added approximate calculator for BC and Canada taxes

--- src/components/calculatorcard.tsx
+++ src/components/calculatorcard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import MoneyInput from "@/components/moneyinput"
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
+import { BASE_CPP_RATE, BC_TAX_BRACKETS, CANADA_TAX_BRACKETS, CPP_RATE, EI_RATE, ENHANCED_CPP_RATE, MAX_CPP_CONTRIBUTORY_EARNINGS, MAX_EI_CONTRIBUTORY_EARNINGS } from "@/lib/utils";
 
 function CalculatorCard() {
   const [employmentIncome, setEmploymentIncome] = useState(0);
@@ -8,6 +9,14 @@ function CalculatorCard() {
   const [rrspfhsaDeductions, setRrspfhsaDeductions] = useState(0);
   const [otherIncome, setOtherIncome] = useState(0);
   const [taxesPaid, setTaxesPaid] = useState(0);
+
+  const [cppTaxesOwed, setCppTaxesOwed] = useState("");
+  const [eiTaxesOwed, setEiTaxesOwed] = useState("");
+  const [bcTaxesOwed, setBcTaxesOwed] = useState("");
+  const [canadaTaxesOwed, setCanadaTaxesOwed] = useState("");
+  const [totalTaxesOwed, setTotalTaxesOwed] = useState("");
+
+  const [taxesFinal, setTaxesFinal] = useState("");
 
   const updateEmploymentIncome = (event: ChangeEvent<HTMLInputElement>) => {
     let parsed = parseInt(event.target.value)
@@ -55,8 +64,59 @@ function CalculatorCard() {
   }
 
   const calculateTaxes = () => {
-    
+    // Add up to get the total taxible income
+    let totalTaxableIncome = (employmentIncome + selfEmploymentIncome + otherIncome) - (rrspfhsaDeductions)
+
+    let cppEnhancedDeduction = totalTaxableIncome > MAX_CPP_CONTRIBUTORY_EARNINGS ? (MAX_CPP_CONTRIBUTORY_EARNINGS * ENHANCED_CPP_RATE) : (totalTaxableIncome * ENHANCED_CPP_RATE)
+    let cppBaseTaxCredit = totalTaxableIncome > MAX_CPP_CONTRIBUTORY_EARNINGS ? (MAX_CPP_CONTRIBUTORY_EARNINGS * BASE_CPP_RATE) : (totalTaxableIncome * BASE_CPP_RATE)
+    let ei = totalTaxableIncome > MAX_EI_CONTRIBUTORY_EARNINGS ? (MAX_EI_CONTRIBUTORY_EARNINGS * EI_RATE) : (totalTaxableIncome * EI_RATE)
+
+    let canadaTaxes = 0
+    let bcTaxes = 0
+
+    let totalTaxableIncomeCanada = totalTaxableIncome - cppEnhancedDeduction
+    let canadaRefundableTaxCredits = (15000 + cppBaseTaxCredit + ei + (Math.min((employmentIncome + otherIncome), 1368))) * 0.15
+
+    let totalTaxableIncomeBC = totalTaxableIncome - cppEnhancedDeduction
+    let bcNonRefundableTaxCredits = (11981 + cppBaseTaxCredit + ei) * 0.0506
+
+    CANADA_TAX_BRACKETS.forEach(bracket => {
+      let amountForBracket = bracket.max - bracket.min;
+      let amountTaxedInBracket = Math.min(totalTaxableIncomeCanada, amountForBracket)
+
+      totalTaxableIncomeCanada -= amountTaxedInBracket
+
+      if (amountTaxedInBracket > 0) {
+        canadaTaxes += (amountTaxedInBracket * bracket.rate)
+      }
+    });
+
+    BC_TAX_BRACKETS.forEach(bracket => {
+      let amountForBracket = bracket.max - bracket.min;
+      let amountTaxedInBracket = Math.min(totalTaxableIncomeBC, amountForBracket)
+
+      totalTaxableIncomeBC -= amountTaxedInBracket
+
+      if (amountTaxedInBracket > 0) {
+        bcTaxes += amountTaxedInBracket * bracket.rate
+      }
+    });
+
+    canadaTaxes -= canadaRefundableTaxCredits
+    bcTaxes = ((bcTaxes - bcNonRefundableTaxCredits) < 0) ? 0 : (bcTaxes - bcNonRefundableTaxCredits)
+    let allTaxes = canadaTaxes + bcTaxes + cppBaseTaxCredit + cppEnhancedDeduction + ei
+
+    setCppTaxesOwed((cppBaseTaxCredit + cppEnhancedDeduction).toFixed(2))
+    setEiTaxesOwed(ei.toFixed(2))
+    setCanadaTaxesOwed(canadaTaxes.toFixed(2))
+    setBcTaxesOwed(bcTaxes.toFixed(2))
+    setTotalTaxesOwed(allTaxes.toFixed(2))
+    setTaxesFinal((allTaxes - taxesPaid).toFixed(2))
   }
+
+  useEffect(() => {
+    calculateTaxes()
+  }, [employmentIncome, selfEmploymentIncome, otherIncome, rrspfhsaDeductions, taxesPaid])
 
   return (
     <Card>
@@ -65,7 +125,7 @@ function CalculatorCard() {
           Calculate BC Taxes
         </CardTitle>
         <CardDescription>
-          Calculate taxes in line with the 2024 Government of Canada and Government of BC tax brackets
+          Calculate approximate owed taxes in line with the 2024 Government of Canada and Government of BC tax brackets
         </CardDescription>
       </CardHeader>
       <CardContent>
@@ -75,8 +135,36 @@ function CalculatorCard() {
         <MoneyInput name="other-income" placeholder="Other Income" value={otherIncome === 0 ? "" : otherIncome} onChange={updateOtherIncome} />
         <MoneyInput name="taxes-paid" placeholder="Taxes Paid" value={taxesPaid === 0 ? "" : taxesPaid} onChange={updateTaxesPaid} />
       </CardContent>
-      <CardFooter>
-        <p>Card footer</p>
+      <CardFooter className="block">
+        <div className="flex col-span-2 justify-between mb-2">
+          <p>BC Tax Owed</p>
+          <p>${bcTaxesOwed}</p>
+        </div>
+        <div className="flex col-span-2 justify-between mb-2">
+          <p>Canada Tax Owed</p>
+          <p>${canadaTaxesOwed}</p>
+        </div>
+        <div className="flex col-span-2 justify-between mb-2">
+          <p>CPP Owed</p>
+          <p>${cppTaxesOwed}</p>
+        </div>
+        <div className="flex col-span-2 justify-between mb-2">
+          <p>EI Owed</p>
+          <p>${eiTaxesOwed}</p>
+        </div>
+        <hr className="border-white mb-2"></hr>
+        <div className="flex col-span-2 justify-between mb-2">
+          <p>Total Tax Owed</p>
+          <p>${totalTaxesOwed}</p>
+        </div>
+        <div className="flex col-span-2 justify-between mb-2">
+          <p>Tax Already Paid</p>
+          <p>- ${taxesPaid.toFixed(2)}</p>
+        </div>
+        <div className="flex col-span-2 justify-between mb-2">
+          <p>Total Taxes Owed</p>
+          <p>= ${taxesFinal}</p>
+        </div>
       </CardFooter>
     </Card>
   )
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,52 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const BC_TAX_BRACKETS = [
+  {
+    min: 0, max: 47937, rate: 0.0506 
+  },
+  {
+    min: 47937.01, max: 95875, rate: 0.0770
+  },
+  {
+    min: 95875.01, max: 110076, rate: 0.1050
+  },
+  {
+    min: 110076.01, max: 133664, rate: 0.1229
+  },
+  {
+    min: 133664.01, max: 181232, rate: 0.1470
+  },
+  {
+    min: 181232.01, max: 252752, rate: 0.1680
+  },
+  {
+    min: 252752.01, max: 999999, rate: 0.2050
+  }
+]
+
+export const CANADA_TAX_BRACKETS = [
+  {
+    min: 0, max: 55867, rate: 0.1500
+  },
+  {
+    min: 55867.01, max: 111733, rate: 0.2050
+  },
+  {
+    min: 111733.01, max: 173205, rate: 0.2600
+  },
+  {
+    min: 173205.01, max: 246752, rate: 0.2900
+  },
+  {
+    min: 246752.01, max: 999999, rate: 0.3300
+  }
+]
+
+export const MAX_CPP_CONTRIBUTORY_EARNINGS = 65000
+export const BASE_CPP_RATE = 0.0495
+export const ENHANCED_CPP_RATE = 0.0100
+
+export const MAX_EI_CONTRIBUTORY_EARNINGS = 63200
+export const EI_RATE = 0.0166
